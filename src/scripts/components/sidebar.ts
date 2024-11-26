db0fc7281b83db6d85060c9bbc109026e33b3720
Dmitry Conquer <dmitry.conquer@gmail.com>
2024-11-26 16:51:26 +0200

    All pages + AOS

---
+++ src/scripts/components/sidebar.ts
@@ -0,0 +1,41 @@
+type TypeSidebarOptions = {
+  buttonId: string;
+  sidebarId: string;
+};
+export default class Sidebar {
+  private button: HTMLButtonElement;
+  private sidebar: HTMLElement;
+  private options: TypeSidebarOptions;
+  private state: boolean;
+  constructor(options: TypeSidebarOptions) {
+    this.state = false;
+    this.options = options;
+    this.button = document.getElementById(this.options.buttonId) as HTMLButtonElement;
+    this.sidebar = document.getElementById(this.options.sidebarId) as HTMLElement;
+  }
+  public init() {
+    if (!this.button || !this.sidebar) {
+      return;
+    }
+    this.clickOutside();
+    this.initListeners();
+  }
+
+  private initListeners() {
+    this.button?.addEventListener("click", () => this.toggle());
+  }
+  private clickOutside() {
+    document.body.addEventListener("click", (e: Event) => {
+      const target = e.target as Element | null;
+      const isInsideSidebar = target?.closest(`#${this.options.sidebarId}`);
+      if (!isInsideSidebar && this.state) {
+        this.toggle();
+      }
+    });
+  }
+
+  private toggle() {
+    this.state = !this.state;
+    document.body.classList.toggle("sidebar-is-open");
+  }
+}
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,10 +1,60 @@
 import "../styles/style.scss";
-
+import { Loader } from "@googlemaps/js-api-loader";
 import { Header } from "./components/header";
 import { Slider } from "./components/sliders";
+import Sidebar from "./components/sidebar";
 
 document.addEventListener("DOMContentLoaded", () => {
+  /* Header */
   const header = new Header({});
   header.init();
+
+  /* Sliders */
   Slider();
+
+  /* Sidebar */
+  const sidebar = new Sidebar({
+    buttonId: "sidebar-button",
+    sidebarId: "sidebar",
+  });
+  sidebar.init();
+
+  /* AOS */
+  // @ts-ignore
+  AOS.init({
+    duration: 800,
+    once: true,
+  });
+
+  /* Google map */
+  const loader = new Loader({
+    apiKey: "AIzaSyBarQT_vJuw5uJhG66aCYYsjSjTXOq8th0",
+    version: "weekly",
+  });
+
+  const initMap = (): void => {
+    loader.importLibrary("maps").then(({ Map }) => {
+      let map = new Map(document.getElementById("map") as HTMLElement, {
+        center: {
+          lat: 39.8283,
+          lng: -98.5795,
+        },
+        zoom: 3,
+        mapId: "3ee05f8926fee9b3",
+      });
+      map.data.loadGeoJson("/coords.json");
+    });
+    // loader.importLibrary("marker").then(({ AdvancedMarkerElement }) => {
+    //   markersList.forEach(m => {
+    //     const marker = new AdvancedMarkerElement({
+    //       map,
+    //       position: { lat: m.position.lat, lng: m.position.lng },
+    //       content: buildContent(m.content),
+    //       title: "TITLE",
+    //     });
+    //   });
+    // });
+  };
+
+  initMap();
 });
