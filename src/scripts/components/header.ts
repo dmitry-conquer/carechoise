6ed073a220da20302e6e748b4b5f609fbe704e04
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-08-22 14:25:00 +0300

    Оновлення заголовка: додано підменю для пунктів меню з дітьми, покращено стилі для відображення підменю, а також внесено зміни в JavaScript для обробки взаємодії з меню.

--- src/scripts/components/header.ts
+++ src/scripts/components/header.ts
@@ -8,6 +8,8 @@ export class Header {
   private settings: HeaderOptions;
   private button: HTMLButtonElement;
   private menu: HTMLElement;
+  private itemHasSubmenuElements: HTMLElement[];
+
   constructor(options?: Partial<HeaderOptions>) {
     this.defaults = {
       buttonId: "open-menu-button",
@@ -16,6 +18,7 @@ export class Header {
     this.settings = { ...this.defaults, ...options };
     this.button = document.getElementById(this.settings.buttonId) as HTMLButtonElement;
     this.menu = document.getElementById(this.settings.menuId) as HTMLButtonElement;
+    this.itemHasSubmenuElements = Array.from(this.menu.querySelectorAll(".menu-item-has-children"));
   }
   private preventInit(): boolean {
     if (this.button && this.menu) {
@@ -31,9 +34,61 @@ export class Header {
     }
     this.initListeners();
   }
-  private initListeners() {
-    this.button.addEventListener("click", () => this.toggleMenu());
+
+  private onDocumentClick = (e: MouseEvent): void => {
+    const target = e.target as HTMLElement;
+    if (target.closest(this.button.id) || target.closest(this.menu.id) || target.closest(".menu-item-has-children")) return;
+    this.itemHasSubmenuElements.forEach(menuItem => {
+      const subMenu = menuItem.querySelector("ul") as HTMLElement;
+      if (!subMenu) return;
+      menuItem?.classList.remove("is-active");
+      subMenu.style.maxHeight = "";
+    });
+  };
+
+  private toggleSubmenu(currentIndex: number): void {
+    this.itemHasSubmenuElements.forEach((menuItem, index) => {
+      const subMenu = menuItem.querySelector("ul") as HTMLElement;
+      if (!subMenu) return;
+      const active = menuItem.classList.contains("is-active");
+
+      if (currentIndex === index) {
+        menuItem?.classList.toggle("is-active");
+        subMenu.style.maxHeight = active ? "" : `${subMenu.scrollHeight}px`;
+      } else {
+        menuItem.classList.remove("is-active");
+        subMenu.style.maxHeight = "";
+      }
+    });
   }
+
+  private handleInteraction(index: number, e: MouseEvent) {
+    const menuItem = this.itemHasSubmenuElements[index];
+    const isActive = menuItem.classList.contains("is-active");
+
+    if (!isActive) {
+      e.preventDefault();
+      this.toggleSubmenu(index);
+    }
+  }
+
+  private initListeners() {
+    document.addEventListener("click", this.onDocumentClick);
+    this.button.addEventListener("click", () => this.toggleMenu());
+    this.itemHasSubmenuElements.forEach((item, index) => {
+      (item.querySelector(":scope > a") as HTMLElement | null)?.addEventListener("click", (event: MouseEvent) => {
+        if (this.isTouchDevice) {
+          console.log(item);
+          this.handleInteraction(index, event);
+        }
+      });
+    });
+  }
+
+  get isTouchDevice() {
+    return navigator.maxTouchPoints > 0 || window.matchMedia?.("(pointer: coarse)")?.matches;
+  }
+
   private toggleMenu() {
     document.body.classList.toggle("menu-is-open");
     this.button.classList.toggle("menu-is-open");
