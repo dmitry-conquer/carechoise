91fcaed89b8a81a0b1d63e3cb53a60b9beb2408d
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-09-12 12:13:28 +0300

    Видалення непотрібних файлів: .stylelintrc, example.js, package-lock.json, footer.html, header.html, modal.html, burger.scss, footer.scss, functions.scss, header.scss, helpers.scss, а також оновлення package.json та vite.config.js для покращення конфігурації проекту.

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,90 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
-import viteJoinMediaQueries from "vite-join-media-queries";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Constants
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const PAGES_DIR = path.resolve(__dirname, "pages");
+const COMPONENTS_DIR = path.resolve(__dirname, "components");
+const SRC_DIR = path.resolve(__dirname, "src");
+
+// Build input configuration
+const buildInput = {};
+fs.readdirSync(PAGES_DIR).forEach(file => {
+  if (file.endsWith(".html")) {
+    const name = path.parse(file).name;
+    buildInput[name] = path.resolve(PAGES_DIR, file);
+  }
+});
+buildInput.main = path.resolve(SRC_DIR, "scripts/main.ts");
+
+// CSS configuration
+const cssConfig = {
+  devSourcemap: true,
+  preprocessorOptions: {
+    scss: {
+      api: "modern-compiler",
+    },
+  },
+};
+
+// Build configuration
+const buildConfig = {
+  appType: "mpa",
+  publicDir: "public",
+  modulePreload: false,
+  rollupOptions: {
+    input: buildInput,
+    output: {
+      entryFileNames: "assets/[name].js",
+      assetFileNames: ({ name }) => {
+        if (/\.(css|scss|sass|less|styl|stylus)$/.test(name ?? "")) {
+          return "assets/style[extname]";
+        }
+        return "assets/[name][extname]";
+      },
+    },
+  },
+};
+
+// Custom plugins
+const plugins = [
+  handlebars({
+    partialDirectory: COMPONENTS_DIR,
+  }),
+  {
+    name: "handlebars-watcher",
+    configureServer(server) {
+      const watcher = server.watcher;
+      watcher.add([`${COMPONENTS_DIR}/**/*.html`]);
+
+      watcher.on("change", file => {
+        if (file.endsWith(".html")) {
+          server.ws.send({
+            type: "full-reload",
+            path: "*",
+          });
+        }
+      });
+    },
+  },
+  {
+    name: "wrap-in-iife",
+    generateBundle(outputOptions, bundle) {
+      Object.keys(bundle).forEach(fileName => {
+        const file = bundle[fileName];
+        if (fileName.includes("main.js") && "code" in file) {
+          file.code = `(() => {\n${file.code}})();`;
+        }
+      });
+    },
+  },
+];
 
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler",
-      },
-    },
-  },
-  build: {
-    modulePreload: false,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "pages/index.html"),
-      },
-      output: {
-        assetFileNames: ({ name }) => {
-          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? "")) {
-            return "assets/images/[name]-[hash][extname]";
-          }
-          return "assets/[name]-[hash][extname]";
-        },
-      },
-    },
-  },
-  plugins: [
-    viteJoinMediaQueries(),
-    handlebars({
-      partialDirectory: resolve(__dirname, "partials"),
-    }),
-  ],
+  css: cssConfig,
+  build: buildConfig,
+  plugins: plugins,
 });
